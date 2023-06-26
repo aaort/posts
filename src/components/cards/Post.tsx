da6c4744071491d02650b69bb77c445b9e7aaa01
aort <artashesano@gmail.com>
2023-06-26 15:13:40 +0400

    Fetch user data in Post

--- src/components/cards/Post.tsx
+++ src/components/cards/Post.tsx
@@ -1,42 +1,55 @@
 import { styled } from '../../theme';
 import type { Post as PostType } from '../../types';
+import { fetcher } from '../../utils';
 import Box from '../Box';
-import { Column, Row } from '../common';
+import { Row } from '../common';
+import useSWR from 'swr';
+import type { User } from '../../types';
 
 type PostProps = {
   post: PostType;
 };
 
 const Post: React.FC<PostProps> = ({ post }) => {
+  const endpoint = `${process.env.REACT_APP_API_BASE_URL}users/${post.userId}`;
+  const { data, error, isLoading } = useSWR('api/user', () =>
+    fetcher(endpoint)
+  );
+
+  const user: User = data;
+
   return (
-    <Box>
-      <Container>
-        <Row>
-          <span> Lorem, ipsum dolor.</span> | <span>@aaort</span>
-        </Row>
-        <div>
-          <p>{post.body}</p>
-        </div>
-        <div
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            gap: '16px',
-            justifyContent: 'flex-end',
-          }}
-        >
-          <span>Comment</span>
-          <span>Edit</span>
-          <span>Favorite</span>
-          <span>Delete</span>
-        </div>
-      </Container>
-    </Box>
+    <Container>
+      <Row>
+        <Title> Lorem, ipsum dolor.</Title> | <span>{user.name}</span>
+      </Row>
+      <p>{post.body}</p>
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          gap: '16px',
+          justifyContent: 'flex-end',
+        }}
+      >
+        <span>Comment</span>
+        <span>Edit</span>
+        <span>Favorite</span>
+        <span>Delete</span>
+      </div>
+    </Container>
   );
 };
 
-const Container = styled(Column, {
+const Container = styled(Box, {
+  display: 'flex',
+  flexDirection: 'column',
   gap: '$2',
+  color: '$background',
+});
+
+const Title = styled('span', {
+  fontSize: '$2',
 });
 
 export default Post;
