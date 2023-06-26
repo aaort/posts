9b8eb4eeadbfd65d8961f3e96f0da5b38ca699c7
aort <artashesano@gmail.com>
2023-06-26 17:29:37 +0400

    Implement Album component

--- src/components/cards/Album.tsx
+++ src/components/cards/Album.tsx
@@ -1,7 +1,41 @@
+import useSWR from 'swr';
+import { styled } from '../../theme';
+import type { Album as AlbumType, User } from '../../types';
+import { fetcher } from '../../utils';
+import { Error } from '../common';
 import Box from '../common/Box';
+import Loading from '../common/Loading';
 
-const Album: React.FC<{}> = () => {
-  return <Box>Hello World </Box>;
+type AlbumProps = {
+  album: AlbumType;
 };
+
+const Album: React.FC<AlbumProps> = (props) => {
+  const ownerId = props.album.userId;
+  const endpoint = `${process.env.REACT_APP_API_BASE_URL}users/${ownerId}`;
+  const { data, error, isLoading } = useSWR(`/api/users/${ownerId}`, () =>
+    fetcher(endpoint)
+  );
+
+  if (error) {
+    return <Error />;
+  }
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  const owner = data as User;
+
+  return (
+    <Container>
+      {props.album.title} | {`@${owner.username}`}
+    </Container>
+  );
+};
+
+const Container = styled(Box, {
+  color: '$background',
+});
 
 export default Album;
