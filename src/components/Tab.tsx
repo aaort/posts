79a53d9b8b6f4f11669b2424a5914ceebf884d1d
aort <artashesano@gmail.com>
2023-06-26 12:43:20 +0400

    Implement initial versions of Tabs and Tab components

--- src/components/Tab.tsx
+++ src/components/Tab.tsx
@@ -1,5 +1,19 @@
-const Tab = () => {
-  return <div></div>;
+import { Item as NavigationItem } from '@radix-ui/react-navigation-menu';
+import { styled } from '../theme';
+
+type TabTypes = {
+  title: string;
 };
+
+const Tab: React.FC<TabTypes> = (props) => {
+  const handleClick = () => {};
+
+  return <Item onClick={handleClick}>{props.title}</Item>;
+};
+
+const Item = styled(NavigationItem, {
+  cursor: 'pointer',
+  fontSize: '$2',
+});
 
 export default Tab;
--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -1,5 +1,33 @@
+import * as NavigationMenu from '@radix-ui/react-navigation-menu';
+import { styled } from '../theme';
+
+import Tab from './Tab';
+
 const Tabs = () => {
-  return <div></div>;
+  return (
+    <Root>
+      <List>
+        <Tab title="Posts" />
+        <Tab title="Photos" />
+        <Tab title="Tasks" />
+      </List>
+    </Root>
+  );
 };
+
+const Root = styled(NavigationMenu.Root, {
+  display: 'flex',
+  justifyContent: 'center',
+  width: '20rem',
+});
+
+const List = styled(NavigationMenu.List, {
+  display: 'flex',
+  width: '20rem',
+  justifyContent: 'center',
+  gap: '$2',
+  listStyle: 'none',
+  padding: 0,
+});
 
 export default Tabs;
