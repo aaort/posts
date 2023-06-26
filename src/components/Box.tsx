9c0360547b8edcd7174487d93338d0e718614be0
aort <artashesano@gmail.com>
2023-06-26 15:11:50 +0400

    Customizations on Box

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -1,12 +1,6 @@
 import { styled } from '../theme';
 
-type BoxTypes = React.PropsWithChildren & {};
-
-const Box: React.FC<BoxTypes> = ({ children }) => {
-  return <Container>{children}</Container>;
-};
-
-const Container = styled('div', {
+const Box = styled('div', {
   padding: '$2',
   backgroundColor: '$card',
 });
