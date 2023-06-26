46fcd807f42c17f8866eab86bd3d1ef9e971cd99
aort <artashesano@gmail.com>
2023-06-26 17:29:11 +0400

    Export all common components from /common/index

--- src/components/common/index.tsx
+++ src/components/common/index.tsx
@@ -1,4 +1,9 @@
-import Row from './Row';
+import Box from './Box';
+import Button from './Button';
 import Column from './Column';
+import Error from './Error';
+import Loading from './Loading';
+import Row from './Row';
 
-export { Row, Column };
+export { Box, Button, Column, Error, Loading, Row };
+
