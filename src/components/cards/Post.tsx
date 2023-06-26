1764a21de2c57ac58ef93171a20f5d8ddb90fbf6
aort <artashesano@gmail.com>
2023-06-26 13:33:13 +0400

    Add initial visual structure of Post component

--- src/components/cards/Post.tsx
+++ src/components/cards/Post.tsx
@@ -1,7 +1,42 @@
+import { styled } from '../../theme';
+import type { Post as PostType } from '../../types';
 import Box from '../Box';
+import { Column, Row } from '../common';
 
-const Post: React.FC<{}> = () => {
-  return <Box>Hello World </Box>;
+type PostProps = {
+  post: PostType;
 };
+
+const Post: React.FC<PostProps> = ({ post }) => {
+  return (
+    <Box>
+      <Container>
+        <Row>
+          <span> Lorem, ipsum dolor.</span> | <span>@aaort</span>
+        </Row>
+        <div>
+          <p>{post.body}</p>
+        </div>
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            gap: '16px',
+            justifyContent: 'flex-end',
+          }}
+        >
+          <span>Comment</span>
+          <span>Edit</span>
+          <span>Favorite</span>
+          <span>Delete</span>
+        </div>
+      </Container>
+    </Box>
+  );
+};
+
+const Container = styled(Column, {
+  gap: '0.5rem',
+});
 
 export default Post;
