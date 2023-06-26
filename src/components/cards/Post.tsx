213e8073d6cdaa079e3ee9a89e3aa99552f8e9b2
aort <artashesano@gmail.com>
2023-06-26 15:34:30 +0400

    Handle user being falsy case in Post

--- src/components/cards/Post.tsx
+++ src/components/cards/Post.tsx
@@ -12,16 +12,21 @@ type PostProps = {
 
 const Post: React.FC<PostProps> = ({ post }) => {
   const endpoint = `${process.env.REACT_APP_API_BASE_URL}users/${post.userId}`;
-  const { data, error, isLoading } = useSWR('api/user', () =>
+  const { data, error, isLoading } = useSWR(`api/user/${post.userId}`, () =>
     fetcher(endpoint)
   );
 
   const user: User = data;
 
+  // Handle edge cases when user is falsy value
+  if (!user) {
+    return null;
+  }
+
   return (
     <Container>
-      <Row>
-        <Title> Lorem, ipsum dolor.</Title> | <span>{user.name}</span>
+      <Row css={{ gap: '$1' }}>
+        <Title>{post.title}</Title> | <Username>{user.username}</Username>
       </Row>
       <p>{post.body}</p>
       <div
@@ -50,6 +55,12 @@ const Container = styled(Box, {
 
 const Title = styled('span', {
   fontSize: '$2',
+  maxLines: 1,
+});
+
+const Username = styled('span', {
+  fontSize: 'calc($2 - 0.4rem)',
+  color: '$gray4',
 });
 
 export default Post;
