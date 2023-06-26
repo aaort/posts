3c8a659c828a92c97026a77ff50870c40baace57
aort <artashesano@gmail.com>
2023-06-26 15:49:37 +0400

    Implement Button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,20 @@
+import { styled } from '../theme';
+
+type ButtonProps = {
+  title: string;
+  handleClick: () => void;
+};
+
+const Button: React.FC<ButtonProps> = (props) => {
+  return <Container onClick={props.handleClick}>{props.title}</Container>;
+};
+
+const Container = styled('button', {
+  backgroundColor: '$background',
+  padding: '$1 $2',
+  border: 'none',
+  fontSize: '$1',
+  borderRadius: '$medium',
+});
+
+export default Button;
