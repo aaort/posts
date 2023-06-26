69a4b70a1c5ca3d57535facedd1f522a85e2ac36
aort <artashesano@gmail.com>
2023-06-26 15:34:16 +0400

    Add utils and colors in theme config

--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -14,6 +14,14 @@ export const {
     colors: {
       background: '#f8f9fa',
       card: '#343a40',
+      gray1: '#f1f3f5',
+      gray2: '#e9ecef',
+      gray3: '#dee2e6',
+      gray4: '#ced4da',
+      gray5: '#adb5bd',
+      gray6: '#868e96',
+      gray7: '#495057',
+      gray9: '#212529',
     },
     space: {
       1: '0.5rem',
@@ -34,9 +42,14 @@ export const {
   media: {
     bp1: '(min-width: 480px)',
   },
-  //   utils: {
-  //     marginX: (value) => ({ marginLeft: value, marginRight: value }),
-  //   },
+  utils: {
+    ml: (value: number | string) => ({ marginLeft: value }),
+    mr: (value: number | string) => ({ marginRight: value }),
+    mx: (value: number | string) => ({ marginInline: value }),
+    my: (value: number | string) => ({ marginBlock: value }),
+    px: (value: number | string) => ({ paddingInline: value }),
+    py: (value: number | string) => ({ paddingBlock: value }),
+  },
 });
 
 export const globalStyles = globalCss({
