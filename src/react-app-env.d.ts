ee3f1a179c9313b833881a8c067841697a4e6394
aort <artashesano@gmail.com>
2023-06-26 15:11:33 +0400

    Add api base url env variable

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,9 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    //types of envs
+    NODE_ENV: 'development' | 'production' | 'test';
+    REACT_APP_API_BASE_URL: string;
+  }
+}
+
 /// <reference types="react-scripts" />
