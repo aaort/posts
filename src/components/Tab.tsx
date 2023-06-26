bcd0b3df342e4ed1605fff121ab3ff14d12c3d51
aort <artashesano@gmail.com>
2023-06-26 16:41:47 +0400

    Make Tabs selectable

--- src/components/Tab.tsx
+++ src/components/Tab.tsx
@@ -1,19 +1,32 @@
 import { Item as NavigationItem } from '@radix-ui/react-navigation-menu';
 import { styled } from '../theme';
+import { Dispatch, SetStateAction } from 'react';
+import type { Tab as TabType } from '../types';
 
 type TabTypes = {
-  title: string;
+  title: TabType;
+  setSelectedTab: Dispatch<SetStateAction<TabType>>;
+  selected: boolean;
 };
 
 const Tab: React.FC<TabTypes> = (props) => {
-  const handleClick = () => {};
+  const handleClick = () => props.setSelectedTab(props.title);
 
-  return <Item onClick={handleClick}>{props.title}</Item>;
+  return (
+    <Item
+      css={{ textDecoration: props.selected ? 'underline' : 'none' }}
+      onClick={handleClick}
+    >
+      {props.title}
+    </Item>
+  );
 };
 
 const Item = styled(NavigationItem, {
   cursor: 'pointer',
   fontSize: '$2',
+  textTransform: 'uppercase',
+  textUnderlineOffset: '0.5rem',
 });
 
 export default Tab;
--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -1,15 +1,26 @@
 import * as NavigationMenu from '@radix-ui/react-navigation-menu';
 import { styled } from '../theme';
-
+import type { Tab as TabType } from '../types';
 import Tab from './Tab';
 
-const Tabs = () => {
+type TabsProps = {
+  selectedTab: TabType;
+  setSelectedTab: React.Dispatch<React.SetStateAction<TabType>>;
+};
+
+const tabs: TabType[] = ['posts', 'photos', 'tasks'];
+
+const Tabs: React.FC<TabsProps> = (props) => {
   return (
     <Root>
       <List>
-        <Tab title="Posts" />
-        <Tab title="Photos" />
-        <Tab title="Tasks" />
+        {tabs.map((tab) => (
+          <Tab
+            title={tab}
+            setSelectedTab={props.setSelectedTab}
+            selected={props.selectedTab === tab}
+          />
+        ))}
       </List>
     </Root>
   );
@@ -25,7 +36,7 @@ const List = styled(NavigationMenu.List, {
   display: 'flex',
   width: '20rem',
   justifyContent: 'center',
-  gap: '$2',
+  gap: '$3',
   listStyle: 'none',
   padding: 0,
 });
