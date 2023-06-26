a4f8e930b92cb1670b2cf0b7cdab00e27206944b
aort <artashesano@gmail.com>
2023-06-26 12:50:08 +0400

    Add Box component

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -1,5 +1,15 @@
-const Box = () => {
-  return <div></div>;
+import { styled } from '../theme';
+
+type BoxTypes = React.PropsWithChildren & {};
+
+const Box: React.FC<BoxTypes> = ({ children }) => {
+  return <Container>{children}</Container>;
 };
+
+const Container = styled('div', {
+  display: 'flex',
+  padding: '$1',
+  backgroundColor: '$card',
+});
 
 export default Box;
