a8f3117f6eaf6fd0e6172f4ca19c4dd67568604f
aort <artashesano@gmail.com>
2023-06-27 17:28:16 +0400

    Add Comment type in /types

--- src/types/index.tsx
+++ src/types/index.tsx
@@ -13,6 +13,14 @@ type User = {
   address: string;
 };
 
+type Comment = {
+  postId: number;
+  id: number;
+  name: string;
+  email: string;
+  body: string;
+};
+
 type Album = Omit<Post, 'body'> & {};
 
 // Custom types used in the project
@@ -20,4 +28,4 @@ type Tab = 'posts' | 'photos' | 'todos';
 
 type Limit = '10' | '20' | '50' | '100';
 
-export type { Album, Limit, Post, Tab, User };
+export type { Album, Comment, Limit, Post, Tab, User };
