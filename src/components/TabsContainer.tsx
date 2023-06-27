b5375cf0e325d016436a8c9f59deb945f297357d
aort <artashesano@gmail.com>
2023-06-27 13:15:18 +0400

    Remove Limit context and its usage

--- src/components/TabsContainer.tsx
+++ src/components/TabsContainer.tsx
@@ -1,8 +1,6 @@
 import Tabs from '@/components/Tabs';
-import LimitContext from '@/context/LImit';
 import { styled } from '@/theme';
-import { Limit, Tab } from '@/types';
-import { getLimit } from '@/utils';
+import { Tab } from '@/types';
 import { Suspense, lazy, useState } from 'react';
 import { Column } from './common';
 import Loading from './common/Loading';
@@ -13,23 +11,20 @@ const Todos = lazy(() => import('./lists/Todos'));
 
 const TabsContainer: React.FC<{}> = () => {
   const [selectedTab, setSelectedTab] = useState<Tab>('posts');
-  const [limit, setLimit] = useState<Limit>(getLimit());
 
   return (
-    <LimitContext.Provider value={[limit, setLimit]}>
-      <Container>
-        <Tabs selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
-        <Suspense fallback={<Loading />}>
-          {selectedTab === 'posts' ? (
-            <Posts />
-          ) : selectedTab === 'photos' ? (
-            <Photos />
-          ) : (
-            <Todos />
-          )}
-        </Suspense>
-      </Container>
-    </LimitContext.Provider>
+    <Container>
+      <Tabs selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
+      <Suspense fallback={<Loading />}>
+        {selectedTab === 'posts' ? (
+          <Posts />
+        ) : selectedTab === 'photos' ? (
+          <Photos />
+        ) : (
+          <Todos />
+        )}
+      </Suspense>
+    </Container>
   );
 };
 
