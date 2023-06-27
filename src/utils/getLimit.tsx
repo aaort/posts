fee52aff29255ae2e6771981d9d1b6e7ce133152
aort <artashesano@gmail.com>
2023-06-27 13:13:58 +0400

    Make getLimit read limit from local storage

--- src/utils/getLimit.tsx
+++ src/utils/getLimit.tsx
@@ -1,7 +1,7 @@
 import { Limit } from '@/types';
 
 const getLimit: () => Limit = () => {
-  return (sessionStorage.getItem('limit') as Limit) ?? ('10' as const);
+  return (localStorage.getItem('limit') as Limit) ?? ('10' as const);
 };
 
 export default getLimit;
