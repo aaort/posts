90c1fcc765affb677a0dd8784305f731f4d76984
aort <artashesano@gmail.com>
2023-06-27 19:32:22 +0400

    Give an opacity to completed todos

--- src/components/cards/Todo.tsx
+++ src/components/cards/Todo.tsx
@@ -1,12 +1,15 @@
 import Box from '@/components/common/Box';
 import { Todo as TodoType } from '@/types';
+import { CSS } from '@stitches/react';
 
 type TodoProps = {
   todo: TodoType;
 };
 
-const Todo: React.FC<TodoProps> = (props) => {
-  return <Box title={props.todo.title} subtitle={''} content={''} />;
+const Todo: React.FC<TodoProps> = ({ todo }) => {
+  const css: CSS = todo.completed ? { opacity: '.5' } : {};
+
+  return <Box title={todo.title} subtitle={''} content={''} css={css} />;
 };
 
 export default Todo;
