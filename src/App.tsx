1a6d7739a2b85541dfc762cd2303ddc3a9b1c348
aort <artashesano@gmail.com>
2023-06-27 13:50:44 +0400

    Pass swr config at the App level

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
+import { SWRConfig, SWRConfiguration } from 'swr';
 import './App.css';
 import Home from './pages';
 import { globalStyles } from './theme';
 
+const swrOptions: SWRConfiguration = {
+  revalidateOnFocus: false,
+};
+
 function App() {
   globalStyles();
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <SWRConfig value={swrOptions}>
+      <div className="App">
+        <Home />
+      </div>
+    </SWRConfig>
   );
 }
 
