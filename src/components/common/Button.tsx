a58e9eec4e02323568d113de918cd908d236ff83
aort <artashesano@gmail.com>
2023-06-27 14:53:42 +0400

    Mutate variants of Button and add two more

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -3,9 +3,13 @@ import { styled } from '@/theme';
 type ButtonProps = {
   title: string;
   handleClick: () => void;
-  type?: 'dangerous' | 'success';
+  type?: ButtonType;
 };
 
+type ButtonType = 'comments' | 'edit' | 'favorite' | 'delete';
+
+// This component is specifically designed to be used as action buttons
+// for Post, Album and Todo components
 const Button: React.FC<ButtonProps> = (props) => {
   return (
     <Container type={props.type} onClick={props.handleClick}>
@@ -23,14 +27,26 @@ const Container = styled('button', {
   borderRadius: '$medium',
   color: '$primary',
   cursor: 'pointer',
+  transition: 'box-shadow 40ms linear',
+  '&:hover': {
+    boxShadow: '$medium',
+  },
   variants: {
     type: {
-      dangerous: {
-        backgroundColor: '$error',
+      comments: {
+        backgroundColor: '$gray6',
         color: '$background',
       },
-      success: {
+      edit: {
+        backgroundColor: '$gray6',
+        color: '$background',
+      },
+      favorite: {
         backgroundColor: '$success',
+        color: '$background',
+      },
+      delete: {
+        backgroundColor: '$error',
         color: '$background',
       },
     },
