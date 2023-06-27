0ac18395bd5347344d0e9a3632e984bfcced6726
aort <artashesano@gmail.com>
2023-06-27 13:13:17 +0400

    Remove getUrlFromEndpoint util

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,4 @@
 import fetcher from './fetcher';
 import getLimit from './getLimit';
-import getUrlFromEndpoint from './getUrlFromEndpoint';
 
-export { fetcher, getLimit, getUrlFromEndpoint };
+export { fetcher, getLimit };
