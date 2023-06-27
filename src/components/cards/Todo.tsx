d8057be8132dc80b8e54aeb48c96c7548737d1a4
aort <artashesano@gmail.com>
2023-06-27 19:17:06 +0400

    Implement Todo card component

--- src/components/cards/Todo.tsx
+++ src/components/cards/Todo.tsx
@@ -1,7 +1,12 @@
 import Box from '@/components/common/Box';
+import { Todo as TodoType } from '@/types';
 
-const Todo: React.FC<{}> = () => {
-  return <Box title={''} subtitle={''} content={''} />;
+type TodoProps = {
+  todo: TodoType;
+};
+
+const Todo: React.FC<TodoProps> = (props) => {
+  return <Box title={props.todo.title} subtitle={''} content={''} />;
 };
 
 export default Todo;
