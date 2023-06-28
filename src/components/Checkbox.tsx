9e9a4c77870be168d45cc478c25fc4fe973c80bb
aort <artashesano@gmail.com>
2023-06-28 13:52:40 +0400

    Add optional tooltip for Checkbox

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,20 +1,28 @@
 import { styled } from '@/theme';
 import * as RadixCheckbox from '@radix-ui/react-checkbox';
 import { CheckIcon } from '@radix-ui/react-icons';
+import { Tooltip } from './common';
 
 type CheckboxProps = {
   checked: boolean;
   onChange: () => void;
+  tooltip?: string;
 };
 
 const Checkbox: React.FC<CheckboxProps> = (props) => {
-  return (
+  const Content = (
     <CheckboxRoot checked={props.checked} onCheckedChange={props.onChange}>
       <CheckboxIndicator>
         <CheckIcon width={25} height={25} />
       </CheckboxIndicator>
     </CheckboxRoot>
   );
+
+  if (props.tooltip) {
+    return <Tooltip text={props.tooltip}>{Content}</Tooltip>;
+  } else {
+    return Content;
+  }
 };
 
 const CheckboxRoot = styled(RadixCheckbox.Root, {
