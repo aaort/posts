faad5a73625ff6493cc5603e1471cd0bcf5a42a5
aort <artashesano@gmail.com>
2023-06-28 21:58:15 +0400

    Export getFavoritePosts from index file

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,15 @@
 import fetcher from './fetcher';
 import getLimit from './getLimit';
-import { getCompletedTodos, getDeletedPosts } from './storage';
+import {
+  getCompletedTodos,
+  getDeletedPosts,
+  getFavoritePosts,
+} from './storage';
 
-export { fetcher, getCompletedTodos, getDeletedPosts, getLimit };
+export {
+  fetcher,
+  getCompletedTodos,
+  getDeletedPosts,
+  getFavoritePosts,
+  getLimit,
+};
