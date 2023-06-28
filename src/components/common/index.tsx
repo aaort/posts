b1282da8ef66166bb4ba70738ce64e515552b5aa
aort <artashesano@gmail.com>
2023-06-28 21:58:04 +0400

    Export IconButton from index file

--- src/components/common/index.tsx
+++ src/components/common/index.tsx
@@ -3,8 +3,19 @@ import Button from './Button';
 import Column from './Column';
 import Dialog from './Dialog';
 import Error from './Error';
+import IconButton from './IconButton';
 import Loading from './Loading';
 import Row from './Row';
 import Tooltip from './Tooltip';
 
-export { Box, Button, Column, Dialog, Error, Loading, Row, Tooltip };
+export {
+  Box,
+  Button,
+  Column,
+  Dialog,
+  Error,
+  IconButton,
+  Loading,
+  Row,
+  Tooltip,
+};
