4c14f45aa27d1c122f5bf6ed369cd13aee9818bd
aort <artashesano@gmail.com>
2023-07-11 20:55:18 +0400

    Make main List component flexible

--- src/components/lists/List.tsx
+++ src/components/lists/List.tsx
@@ -2,10 +2,21 @@ import Column from '@/components/common/Column';
 import { styled } from '@/theme';
 
 const List = styled(Column, {
-  maxWidth: '70%',
   gap: 'calc($5 * 2)',
   mx: 'auto',
   mb: '$2',
+  '@xs': {
+    maxWidth: '100%',
+  },
+  '@sm': {
+    maxWidth: '90%',
+  },
+  '@md': {
+    maxWidth: '80%',
+  },
+  '@lg': {
+    maxWidth: '70%',
+  },
 });
 
 export default List;
